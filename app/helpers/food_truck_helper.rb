874ba32226c20cdfb97e555f1a5085fa755f558b
pringshia <pratik@ringshia.com>
2012-06-27 13:49:32 -0700

    Added helper method for building a distance matrix query URL

--- app/helpers/food_truck_helper.rb
+++ app/helpers/food_truck_helper.rb
@@ -1,2 +1,11 @@
 module FoodTruckHelper
+  def build_query_string(food_trucks)
+    querystring = "http://maps.googleapis.com/maps/api/distancematrix/json?sensor=false&units=imperial&origins=34.0634625,-118.3680602&destinations="
+    
+    food_trucks.each_with_index do |food_truck, index|
+      querystring << (food_truck.latitude + "," + food_truck.longitude)
+    	querystring << "|" unless (index == food_trucks.count - 1)
+    end
+    querystring
+  end
 end
